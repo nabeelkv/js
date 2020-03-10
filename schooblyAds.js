b5b7a679013c09f932737a81e1bbeb3f7ef145f7
nabeelkv <43858092+nabeelkv@users.noreply.github.com>
2020-03-10 21:20:41 +0530

    Update schooblyAds.js

--- schooblyAds.js
+++ schooblyAds.js
@@ -23,6 +23,13 @@ var schooblyAds = [
      adNiche: `Teacher Training`,
      adLanding: `tutorjobsonline.com`,
      adCTAText: `Get 7 Day Trial`
+   },   
+    { 
+     adImage: `https://i.udemycdn.com/course/240x135/2687810_8991_3.jpg`,
+     adLink: `https://1e9dard3zmk66ydr3v83ht9xbb.hop.clickbank.net/`,
+     adTitle: `Data Entry Work Online Jobs - Earn From Home`,
+     adNiche: `Home Business`,
+     adCTAText: `Learn More`
    },
    { 
      adImage: `https://www.sloganseller.com/wp-content/uploads/2014/11/SloganGirl2.jpg`,
@@ -32,5 +39,71 @@ var schooblyAds = [
      adLanding: `sloganseller.com`,
      adCTAText: `Learn More`
    },
+ 
+ //E-business & E-marketing
+
+
+{ 
+     adImage: `https://i.udemycdn.com/course/240x135/703522_0e3f_3.jpg`,
+     adLink: `https://46ad6opgtkkizt5r20r3mdp8pe.hop.clickbank.net/`,
+     adTitle: `Get Paid To Use Facebook, Twitter And Youtube`,
+     adNiche: `Home Business`,
+     adCTAText: `Join Now`
+   },
+   { 
+     adImage: `https://i.udemycdn.com/course/240x135/313360_0668_3.jpg`,
+     adLink: `https://50fd6ijbznfb-s9cdpxcvh9ucy.hop.clickbank.net/`,
+     adTitle: `The 12 Minute Affiliate System - Hot Offer!`,
+     adNiche: `Affiliate Marketing`,
+     adCTAText: `Learn More`
+   },
+   { 
+     adImage: `https://i.udemycdn.com/course/240x135/661030_70d3_5.jpg`,
+     adLink: `https://6f429ei2-goh7ycenit9q81k81.hop.clickbank.net/`,
+     adTitle: `Salehoo Wholesale & Dropship Directory - #1 Affiliate Program`,
+     adNiche: `Dropshipping`,
+     adCTAText: `Learn More`
+   },
+   { 
+     adImage: `https://img.youtube.com/vi/XZteEHUNz1I/mqdefault.jpg`,
+     adLink: `https://6df4fcq3xle63o092je51kcq4f.hop.clickbank.net/`,
+     adTitle: `Super Affiliate System - John Crestani's Autowebinar Funnel`,
+     adNiche: `Affiliate Marketing`,
+     adCTAText: `Learn More`
+   },
+   { 
+     adImage: `https://i.udemycdn.com/course/240x135/501164_5826.jpg`,
+     adLink: `https://55cdajk3uhof-p0adl1g2u5r04.hop.clickbank.net/`,
+     adTitle: `CB University: 70k+ Students - Our Clients Have Earned $3.5 Billion, It's Your Turn!`,
+     adNiche: `Home Business`,
+     adCTAText: `Learn More`
+   },
+   { 
+     adImage: `https://i.udemycdn.com/course/240x135/1729276_e950_2.jpg`,
+     adLink: `https://2c8b4gkfpgkh5u3mqzij3lyp8o.hop.clickbank.net/`,
+     adTitle: `Video Marketing Blaster - Rank Videos On #1 Page Of Google And Youtube`,
+     adNiche: `Social Media Marketing`,
+     adCTAText: `Learn More`
+   },{ 
+     adImage: `https://i.udemycdn.com/course/240x135/630050_7fc3.jpg`,
+     adLink: `https://3dde8nd8tchd-y8kjhtu5uesbz.hop.clickbank.net/`,
+     adTitle: `Zendyx Affiliate System - $1.4+ Epcs!`,
+     adNiche: `Affiliate Marketing`,
+     adCTAText: `Learn More`
+   },
+   { 
+     adImage: `https://i.udemycdn.com/course/240x135/576054_7e88_6.jpg`,
+     adLink: `https://03165ihb0dlh1n4dnbt1xz12sp.hop.clickbank.net/`,
+     adTitle: `Instant Wordpess Theme To Match Your Existing Website Design!`,
+     adNiche: `Web Design`,
+     adCTAText: `Learn More`
+   },
+   { 
+     adImage: `https://i.udemycdn.com/course/240x135/487450_afe7_2.jpg`,
+     adLink: `https://4fe62mn9u9m99r97ult656rnee.hop.clickbank.net/`,
+     adTitle: `Host Unlimited Sites From $3 Per Year!`,
+     adNiche: `Web Design`,
+     adCTAText: `Learn More`
+   }
 
 ];
