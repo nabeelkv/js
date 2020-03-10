a4af9d9aaed4ce962c1dc020c57809b367efb8e4
nabeelkv <43858092+nabeelkv@users.noreply.github.com>
2020-03-10 20:17:36 +0530

    Create schooblyAds.js

---
+++ schooblyAds.js
@@ -0,0 +1,36 @@
+var schooblyAds = [
+ 
+   { 
+     adImage: `https://www.photojobz.com/hosted/images/6e/87624686c849a4a431b54ce4539a51/Story.image.text.6.2-2-.jpg`,
+     adLink: `https://cba52mrfpdqeamc1hdn6mbxvky.hop.clickbank.net/?tid=INFEED`,
+     adTitle: `Earn From Your Photos!`,
+     adNiche: `Photography`,
+     adLanding: `photojobz.co`,
+     adCTAText: `Join Now`
+   },
+   { 
+     adImage: `https://miro.medium.com/max/2000/1*sqMHMa6-2uoWTRkK3EIhHg.jpeg`,
+     adLink: `https://22436eofyfjj2o5gtojl0hpp8g.hop.clickbank.net/?tid=INFEED`,
+     adTitle: `Ultimate Guide To Job Interview Answers`,
+     adNiche: `Career Development`,
+     adLanding: `interview-answers.com`,
+     adCTAText: `Learn More`
+   },
+   { 
+     adImage: `https://1.bp.blogspot.com/-Z0OZ1XjRKMA/XmeX2vOtU-I/AAAAAAAAARI/6WiTAbKNDyMttW2Zuckn6ZtkT0iUQIxywCLcBGAsYHQ/s1600/tutors.png`,
+     adLink: `https://099bdip41bp89r7h-jvnu8qc6l.hop.clickbank.net/`,
+     adTitle: `Tutor Jobs Online | Get Paid To Teach Online`,
+     adNiche: `Teacher Training`,
+     adLanding: `tutorjobsonline.com`,
+     adCTAText: `Get 7 Day Trial`
+   },
+   { 
+     adImage: `https://www.sloganseller.com/wp-content/uploads/2014/11/SloganGirl2.jpg`,
+     adLink: `https://3938cpj8phghcl2cxgw9wei6vx.hop.clickbank.net/`,
+     adTitle: `Slogan Seller - Turn A Phase Into Cash`,
+     adNiche: `Home Business`,
+     adLanding: `sloganseller.com`,
+     adCTAText: `Learn More`
+   },
+
+];
