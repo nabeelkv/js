38008080962f467460a197fd130ea12cf2271a36
nabeelkv <43858092+nabeelkv@users.noreply.github.com>
2020-03-16 21:33:36 +0530

    Create schooblyAds3.js

---
+++ schooblyAds3.js
@@ -0,0 +1,12 @@
+var schooblyAds = [
+ 
+   { 
+     adImage: `https://bluehost-cdn.com/media/partner/images/schoobly/190x60/190x60BW.png`,
+     adLink: `https://www.bluehost.com/track/schoobly/`,
+     adTitle: `Powerful Web Hosting only at $3.95/month with Free Domain + 30-Day Money-Back Guarantee`,
+     adNiche: `Web Hosting`,
+     adLanding: `bluehost.com`,
+     adCTAText: `Get Started`
+   },
+
+];
