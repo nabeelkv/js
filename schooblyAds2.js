f594187d0a75444815e7571fdfe868f7fc4b06f4
nabeelkv <43858092+nabeelkv@users.noreply.github.com>
2020-03-16 21:16:07 +0530

    Update schooblyAds2.js

--- schooblyAds2.js
+++ schooblyAds2.js
@@ -3,7 +3,7 @@ var schooblyAds = [
    { 
      adImage: `https://bluehost-cdn.com/media/partner/images/schoobly/190x60/190x60BW.png`,
      adLink: `https://www.bluehost.com/track/schoobly/`,
-     adTitle: `Powerful Web Hosting only $3.95/month!`,
+     adTitle: `Powerful Web Hosting only $3.95/month with Free Domain & 30-Day Money-Back Guarantee`,
      adNiche: `Web Hosting`,
      adLanding: `bluehost.com`,
      adCTAText: `Get Started`
