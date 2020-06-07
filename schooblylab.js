b9f28084622e129580d5b162cfe8cbde6fc84ce0
nabeelkv <43858092+nabeelkv@users.noreply.github.com>
2020-06-07 16:17:51 +0530

    Create schooblylab.js

---
+++ schooblylab.js
@@ -0,0 +1,26 @@
+#schooblyLab {font-size:15px;line-height:30px;color:black;}
+#schooblyLab a{text-decoration:underline}
+#schooblyLab h3 {margin-top:30px;margin-bottom:10px;}
+#schooblyLab li {margin-left: 16px;}
+#schooblyLab li.wul{list-style:none;background-image: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADAAAAAwCAMAAABg3Am1AAAAWlBMVEUAAAAxokwwoks1pFAxokwxokwxokwxokwxokwnnkQnnkQnnkRou3y84cTS69cxokwonkQxokwnnkRqvH1VsmtluXlVsmsnnkRdtnLw+PIxokwqn0YinEAfmj3goh/UAAAAGnRSTlMA2CcEo+6AQT7+2IOBJxPl27alhoBnX15SCCe258UAAAB+SURBVEjH7dA5EoAgEERR3BcQ923Q+1/T0SqKlNbMouP3gxkRFvZpyQb64VSQT4mOcYc8mU5DnqIG8zXoozj4d34tML+YrET8XBFx4e2F4oAL4N7J3EUB/EfSUwD/zG3hvFdROu9XtL31vgXguQA9F6DnAvM8WbOHpkXYD3cBBCcPjtASYjwAAAAASUVORK5CYII=);
+    background-repeat: no-repeat;
+    background-size: 20px;
+    padding-left: 24px;
+    margin-left: 16px;
+}
+.blog-pager .blog-pager-older-link {
+    background: #00979d;
+    color: white;
+}
+.blog-pager .blog-pager-older-link:hover {
+    background: #00979d;
+    color: white;
+}
+
+#blog-pager {
+    float: left;
+    width: 100%;
+    overflow: hidden;
+    clear: both;
+    margin: 30px 0 10px;
+}
